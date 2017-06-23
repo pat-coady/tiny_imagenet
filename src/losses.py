1a9d7a7d64a82745d512e3c3615ced8a6a278e0f
pat-coady <pcoady00@gmail.com>
2017-06-22 19:45:46 -0600

    add one-vs-all svm

--- src/losses.py
+++ src/losses.py
@@ -39,3 +39,22 @@ def softmax_smooth_ce_loss(logits, labels):
                                             logits,
                                             label_smoothing=0.1)
   tf.summary.scalar('loss', ce_loss)
+
+
+def svm_loss(logits, labels):
+  """SVM loss
+
+  Args:
+    logits: logits (N, C) C = number of classes
+    labels: tf.uint8 labels {0 .. 199}
+
+  Returns:
+    losses: mean cross entropy loss
+
+  """
+  labels = tf.cast(labels, tf.int32)
+  ohe = tf.one_hot(labels, 200, dtype=tf.float32, on_value=-1.0, off_value=1.0)
+  svm_l = tf.reduce_sum(tf.maximum(ohe * logits, 0.0))
+  tf.add_to_collection(tf.GraphKeys.LOSSES, svm_l)
+
+  tf.summary.scalar('loss', svm_l)
